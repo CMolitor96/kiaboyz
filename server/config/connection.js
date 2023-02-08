b5d74e50bc0ad51bad5d7917cedbac1504b197c2
Charles Molitor <Charlie@Charless-MacBook-Pro.local>
2023-02-07 19:20:11 -0800

    Changed env file to root of project so it would work on node start and updated the sql connection to connect to the db thats hosted on AWS now

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,10 +2,10 @@ require('dotenv').config();
 const mysql = require('mysql2');
 const db = mysql.createConnection(
     {
-        host: process.env.REACT_APP_DB_HOST,
-        user: process.env.REACT_APP_DB_USER,
-        password: process.env.REACT_APP_DB_PASSWORD,
-        database: process.env.REACT_APP_DB_NAME,
+        host: process.env.REACT_APP_AWS_DB_HOST,
+        user: process.env.REACT_APP_AWS_DB_USER,
+        password: process.env.REACT_APP_AWS_DB_PASSWORD,
+        database: process.env.REACT_APP_AWS_DB_NAME,
     },
 );
 
