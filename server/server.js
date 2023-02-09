e7518c4287b5783abefe935ac6856ccc7e58ba88
Charles Molitor <Charlie@Charless-MacBook-Pro.local>
2023-02-08 20:24:55 -0800

    Changed code so now works on npm run develop, still having problems with build, idk whats wrong, just 404s on server start up, and problem with api on vercel and amplify. Might try and do webpack to fix the weird not json format error on db query but idk

--- server/server.js
+++ server/server.js
@@ -1,22 +1,22 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const router = require('express').Router();
 const routes = require('./routes');
 const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-// if (process.env.NODE_ENV === 'production') {
-//     app.use(express.static(path.join(__dirname, '../client/build')));
-//   }
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, 'build')));
+  }
 
 // app.use(express.static(path.join(__dirname, 'build')));
-app.use(express.static(path.resolve(__dirname, '../client/build')));
-router.use('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../../client/build/index.html'));
-});
+// app.use(express.static(path.resolve(__dirname, 'build')));
+// app.get('/*', function (req, res) {
+//     res.sendFile(path.join(__dirname, 'build', 'index.html'));
+//   });
+
 
 app.use(routes);
 
