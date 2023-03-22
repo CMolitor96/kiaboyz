11a35cbcd7bd3b3f647177811be139ced3e0b871
Charles Molitor <Charlie@Charless-MacBook-Pro.local>
2023-03-22 15:04:55 -0600

    Added routes to go through index html in build

--- server/server.js
+++ server/server.js
@@ -13,9 +13,9 @@ if (process.env.NODE_ENV === 'production') {
 
 
 // app.use(express.static(path.join(__dirname, '../client/build')));
-// app.get('/*', function (req, res) {
-//   res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
-// });
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+});
 
 
 // app.use(express.static(path.resolve(__dirname, 'build')));
