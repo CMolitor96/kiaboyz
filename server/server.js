87bf5662ab8151559a30598e2f9c40092f1371af
Charles Molitor <Charlie@Charless-MacBook-Pro.local>
2023-06-10 10:36:51 -0500

    Changed around routes on server and index api to actually hit routes, so hopefully works on build now

--- server/server.js
+++ server/server.js
@@ -7,15 +7,15 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, 'build')));
-}
+// if (process.env.NODE_ENV === 'production') {
+//     app.use(express.static(path.join(__dirname, 'build')));
+// }
 
 
 // app.use(express.static(path.join(__dirname, '../client/build')));
-app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
-});
+// app.get('/*', function (req, res) {
+//   res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+// });
 
 
 // app.use(express.static(path.resolve(__dirname, 'build')));
