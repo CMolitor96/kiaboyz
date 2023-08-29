edb6c17985ccd587cf26f6a113845cf92fe4f4b7
Charles Molitor <charlie.molitor.38@gmail.com>
2023-08-29 09:01:45 -0400

    Still going back to months agoRevert "initial commit"
    This reverts commit 66cbb2303766f7373ea3c8c55264699addd4592a.

--- server/server.js
+++ server/server.js
@@ -1,20 +1,27 @@
 const express = require('express');
-const cors = require('cors')
 const path = require('path');
 const app = express();
 const routes = require('./routes');
 const PORT = process.env.PORT || 3001;
 
-app.use(cors())
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, '../client/build')));
+// if (process.env.NODE_ENV === 'production') {
+//     app.use(express.static(path.join(__dirname, 'build')));
+// }
+
+
+// app.use(express.static(path.join(__dirname, '../client/build')));
 // app.get('/*', function (req, res) {
-//     res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+//   res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
 // });
 
 
+// app.use(express.static(path.resolve(__dirname, 'build')));
+// app.get('/*', function (req, res) {
+//     res.sendFile(path.join(__dirname, 'build', 'index.html'));
+//   });
 
 app.use(routes);
 
